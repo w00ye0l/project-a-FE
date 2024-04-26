336137701993301d1042746f2e780e67f9bd5472
이우열 <lwyeol@iuyeol-ui-MacBookAir.local>
2024-04-26 15:46:16 +0900

    feat: 로그인 여부에 따라 컴포넌트 렌더링 분기 추가

--- src/app/_component/MainHeader.tsx
+++ src/app/_component/MainHeader.tsx
@@ -1,7 +1,11 @@
 import style from "@/app/_component/mainHeader.module.css";
 import Link from "next/link";
+import LogoutButton from "./LogoutButton";
+import { auth } from "@/auth";
 
-export default function MainHeader() {
+export default async function MainHeader() {
+  const session = await auth();
+
   return (
     <div className={style.main}>
       <div className={`${style.linkSection} ${style.left}`}>
@@ -16,12 +20,18 @@ export default function MainHeader() {
       <div className={style.logo}>로고</div>
 
       <div className={`${style.linkSection} ${style.right}`}>
-        <Link href="/auth/signup" className={style.link}>
-          회원가입
-        </Link>
-        <Link href="/auth/login" className={style.link}>
-          로그인
-        </Link>
+        {!session?.user ? (
+          <>
+            <Link href="/auth/signup" className={style.link}>
+              회원가입
+            </Link>
+            <Link href="/auth/login" className={style.link}>
+              로그인
+            </Link>
+          </>
+        ) : (
+          <LogoutButton me={session} />
+        )}
       </div>
     </div>
   );
