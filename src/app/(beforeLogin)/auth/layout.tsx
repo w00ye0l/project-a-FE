60e9cfefdc084e333da0f5fe2331a90b3b4a736e
이우열 <lwyeol@iuyeol-ui-MacBookAir.local>
2024-04-26 15:47:08 +0900

    feat: 로그인 후 회원가입, 로그인 페이지 접근 시 홈으로 리다이렉트

--- src/app/(beforeLogin)/auth/layout.tsx
+++ src/app/(beforeLogin)/auth/layout.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import style from "./layout.module.css";
 import Title from "../_component/Title";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function AuthLayout({
+export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/");
+  }
+
   return (
     <div className={style.mainSection}>
       <div className={style.logoSection}>
