8d31661690314263d57af31e2b2ecd71f5478b68
lwyeol <lwyeol1010@azero.biz>
2024-06-11 15:28:34 +0900

    feat: 게시판 생성 기능 추가

--- src/app/(afterLogin)/community/layout.tsx
+++ src/app/(afterLogin)/community/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import BoardList from "./_component/BoardList";
 import NavMenu from "./_component/NavMenu";
 
 export default function CommunityLayout({
@@ -6,10 +8,19 @@ export default function CommunityLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div>
+    <>
       <h1>커뮤니티</h1>
-      <NavMenu />
-      {children}
-    </div>
+
+      <Link href="/">홈으로</Link>
+
+      <div style={{ display: "flex", gap: "30px" }}>
+        <BoardList />
+
+        <div style={{ flex: "10" }}>
+          <NavMenu />
+          {children}
+        </div>
+      </div>
+    </>
   );
 }
