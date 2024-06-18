2c0675a86732ad205fbec452c02c1f38ee99f8ec
lwyeol <lwyeol1010@azero.biz>
2024-06-18 10:26:18 +0900

    feat: 게시글 작성 시 토큰 전달

--- src/app/(afterLogin)/community/_lib/createArticle.ts
+++ src/app/(afterLogin)/community/_lib/createArticle.ts
@@ -1,3 +1,8 @@
+"use server";
+
+import { auth } from "@/auth";
+import { CustomUser } from "@/model/CustomUser";
+
 interface ArticleData {
   title: string;
   content: string;
@@ -5,23 +10,32 @@ interface ArticleData {
 }
 
 interface Props {
-  boardPk: number;
+  boardName: string;
   ArticleData: ArticleData;
 }
 
-export const createArticle = async ({ boardPk, ArticleData }: Props) => {
+export const createArticle = async ({ boardName, ArticleData }: Props) => {
+  const session = await auth();
+
+  // session 객체를 CustomUser 타입으로 캐스팅
+  const user = session as CustomUser;
+
+  // 게시글 작성 API 호출
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/community/${boardPk}`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/community/${boardName}`,
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${user.accessToken}`,
       },
       body: JSON.stringify(ArticleData),
     }
   );
 
   const result = await response.json();
+
+  console.log({ result });
 
   return result;
 };
