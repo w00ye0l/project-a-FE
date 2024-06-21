e4fc9e87fad25bf1ba840f65091203b8ffbd1fbd
lwyeol <lwyeol1010@azero.biz>
2024-06-21 10:02:32 +0900

    fix: 커뮤니티 레이아웃에서 게시글 작성 버튼 제거

--- src/app/(afterLogin)/community/layout.tsx
+++ src/app/(afterLogin)/community/layout.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import BoardList from "./_component/BoardList";
-import WriteNavMenu from "./_component/WriteNavMenu";
 
 export default function CommunityLayout({
   children,
@@ -10,16 +9,12 @@ export default function CommunityLayout({
   return (
     <>
       <h1>커뮤니티</h1>
-
       <Link href="/">홈으로</Link>
 
       <div style={{ display: "flex", gap: "30px" }}>
         <BoardList />
 
-        <div style={{ flex: "10" }}>
-          <WriteNavMenu />
-          {children}
-        </div>
+        <div style={{ flex: "10" }}>{children}</div>
       </div>
     </>
   );
