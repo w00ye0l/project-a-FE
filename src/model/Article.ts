c77a4cc23dcf348a34c18da94422c6f73d065b1f
lwyeol <lwyeol1010@azero.biz>
2024-06-21 09:57:16 +0900

    refactor: 게시글 모델 수정

--- src/model/Article.ts
+++ src/model/Article.ts
@@ -1,12 +1,27 @@
+import { Reaction } from "./Reaction";
+import { Scrap } from "./Scrap";
+import { UserProfile } from "./UserProfile";
+
 export interface Article {
-  nickname: string;
+  user: UserProfile;
+
+  boardPk: number;
   boardName: string;
+
   articlePk: number;
   title: string;
   content: string;
   originContent: string;
+
   readCount: number;
   likeCount: number;
+  neutralCount: number;
+  dislikeCount: number;
+  scrapCount: number;
+
   createdAt: string;
   updatedAt: string;
+
+  reactions: Reaction[];
+  scraps: Scrap[];
 }
