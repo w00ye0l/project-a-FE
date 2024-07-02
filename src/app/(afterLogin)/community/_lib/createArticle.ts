12664c165fa144622feb9cfc92ad79eb5d902a6d
lwyeol <lwyeol1010@azero.biz>
2024-07-02 17:37:49 +0900

    feat: 게시글 작성, 수정 API 요청 시 videos 데이터 추가

--- src/app/(afterLogin)/community/_lib/createArticle.ts
+++ src/app/(afterLogin)/community/_lib/createArticle.ts
@@ -3,18 +3,12 @@
 import { auth } from "@/auth";
 import { CustomUser } from "@/model/CustomUser";
 
-interface ArticleData {
-  title: string;
-  content: string;
-  originContent: string;
-}
-
 interface Props {
   boardName: string;
-  ArticleData: ArticleData;
+  formData: FormData;
 }
 
-export const createArticle = async ({ boardName, ArticleData }: Props) => {
+export const createArticle = async ({ boardName, formData }: Props) => {
   const session = await auth();
 
   // session 객체를 CustomUser 타입으로 캐스팅
@@ -26,10 +20,9 @@ export const createArticle = async ({ boardName, ArticleData }: Props) => {
     {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
         Authorization: `Bearer ${user.accessToken}`,
       },
-      body: JSON.stringify(ArticleData),
+      body: formData,
     }
   );
 
