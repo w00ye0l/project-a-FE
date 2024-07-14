16b29d425e86a9d4615d0122a9ce0f71d7bd20f3
wooyeol <dnduf158@gmail.com>
2024-07-15 00:00:18 +0900

    refactor: main, sub Nav 분리

--- src/app/(afterLogin)/admin/_component/MainNav.tsx
+++ src/app/(afterLogin)/admin/_component/MainNav.tsx
@@ -1,17 +1,45 @@
 "use client";
 
+import { useContext } from "react";
 import style from "./mainNav.module.css";
 import cx from "classnames";
+import { NavContext } from "./NavProvider";
 
 export default function MainNav() {
+  const { mainTab, setMainTab } = useContext(NavContext);
+
+  const onClickCarDefault = () => {
+    setMainTab("carDefault");
+  };
+
+  const onClickCarAddition = () => {
+    setMainTab("carAddition");
+  };
+
   return (
     <>
       <h2 className={style.navTitle}>메뉴</h2>
 
       <nav>
         <ul className={style.mainNav}>
-          <li className={cx(style.mainLi, style.active)}>차량 데이터 관리</li>
-          <li>기본 옵션 관리</li>
+          <li
+            onClick={onClickCarDefault}
+            className={cx(
+              style.mainLi,
+              mainTab === "carDefault" ? style.active : ""
+            )}
+          >
+            기본 옵션 관리
+          </li>
+          <li
+            className={cx(
+              style.mainLi,
+              mainTab === "carAddition" ? style.active : ""
+            )}
+            onClick={onClickCarAddition}
+          >
+            차량 데이터 관리
+          </li>
         </ul>
       </nav>
     </>
