421d054babe3c32ec106fc9f6376cec29a913e68
lwyeol <lwyeol1010@azero.biz>
2024-07-19 18:14:46 +0900

    fix: admin 레이아웃 및 초기 페이지 구조 수정

--- src/app/(afterLogin)/admin/layout.tsx
+++ src/app/(afterLogin)/admin/layout.tsx
@@ -1,8 +1,6 @@
 import MainNav from "./_component/MainNav";
-import NavProvider from "./_component/NavProvider";
 import SubNav from "./_component/SubNav";
 import style from "./admin.module.css";
-import cx from "classnames";
 
 export default function AdminLayout({
   children,
@@ -10,41 +8,23 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <NavProvider>
-      <main className={style.main}>
-        <aside className={style.left}>
-          <h1>ADMIN</h1>
+    <main className={style.main}>
+      <aside className={style.left}>
+        <h1>ADMIN</h1>
 
-          <div className={style.profile}>
-            <div className={style.profileImg}></div>
-            <p>홍길동님</p>
-          </div>
+        <div className={style.profile}>
+          <div className={style.profileImg}></div>
+          <p>홍길동님</p>
+        </div>
 
-          <MainNav />
-        </aside>
+        <MainNav />
+      </aside>
 
-        <section className={style.right}>
-          <SubNav />
+      <section className={style.right}>
+        <SubNav />
 
-          <article className={style.mainSection}>
-            <div className={style.buttonSection}>
-              <div className={style.excelButtonSection}>
-                <button className={cx(style.button, style.excelOpen)}>
-                  엑셀 열기
-                </button>
-                <button className={cx(style.button, style.excelUpload)}>
-                  엑셀 업로드
-                </button>
-              </div>
-              <button className={cx(style.button, style.dbUpload)}>
-                DB 업로드
-              </button>
-            </div>
-
-            <div className={style.mainContainer}>{children}</div>
-          </article>
-        </section>
-      </main>
-    </NavProvider>
+        {children}
+      </section>
+    </main>
   );
 }
