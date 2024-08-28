123dd5713b3448241975d67d2f84b8d4fe1e44ae
lwyeol <lwyeol1010@azero.biz>
2024-08-28 16:25:10 +0900

    feat: 커뮤니티 레이아웃 변경

--- src/app/(afterLogin)/community/layout.tsx
+++ src/app/(afterLogin)/community/layout.tsx
@@ -1,21 +1,55 @@
-import Link from "next/link";
+import { auth } from "@/auth";
 import BoardList from "./_component/BoardList";
+import UserProfile from "./_component/UserProfile";
+import style from "./layout.module.css";
+import cx from "classnames";
+import LoginPanel from "./_component/LoginPanel";
+import Image from "next/image";
+import NoticeList from "./_component/NoticeList";
 
-export default function CommunityLayout({
+export default async function CommunityLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+
   return (
-    <>
-      <h1>커뮤니티</h1>
-      <Link href="/">홈으로</Link>
+    <div className={style.layout}>
+      <BoardList />
 
-      <div style={{ display: "flex", gap: "30px" }}>
-        <BoardList />
+      <div className={style.main}>{children}</div>
 
-        <div style={{ flex: "10" }}>{children}</div>
+      <div className={style.infoPanel}>
+        {session?.user ? <UserProfile me={session} /> : <LoginPanel />}
+
+        <NoticeList />
+
+        <div className={style.adSection}>
+          <div className={cx(style.adContainer, style.smallAd)}>
+            <Image
+              className={style.smallAdImage}
+              src="/ad/440x120.png"
+              width={440}
+              height={120}
+              alt="ad"
+            />
+          </div>
+          <div className={cx(style.adContainer, style.smallAd)}>
+            <Image
+              className={style.smallAdImage}
+              src="/ad/440x120.png"
+              width={440}
+              height={120}
+              alt="ad"
+            />
+          </div>
+        </div>
+
+        <div className={cx(style.adContainer, style.bigAd)}>
+          <Image src="/ad/260x300.png" width={260} height={300} alt="ad" />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
