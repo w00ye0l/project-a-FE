ed10d30ec82374cca74d2a8b06944ca51eaba049
lwyeol <lwyeol1010@azero.biz>
2024-10-08 09:40:57 +0900

    fix: 커뮤니티 댓글, 대댓글 유저 인가 제거

--- src/app/(afterLogin)/community/_lib/getRecommentList.ts
+++ src/app/(afterLogin)/community/_lib/getRecommentList.ts
@@ -1,26 +1,15 @@
 "use server";
 
-import { auth } from "@/auth";
-import { CustomUser } from "@/model/CustomUser";
-
 export const getRecommentList = async ({
   articlePk,
-  commentPk,
 }: {
   articlePk: string;
-  commentPk: number;
 }) => {
-  const session = await auth();
-  const user = session as CustomUser;
-
   // 대댓글 목록 API 호출
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/community/recomment/${articlePk}`,
     {
       method: "GET",
-      headers: {
-        Authorization: `Bearer ${user.accessToken}`,
-      },
     }
   );
 
