bc8426de579b82415dac08fdef5c5a2e86e17fe1
lwyeol <lwyeol1010@azero.biz>
2024-10-21 16:55:17 +0900

    fix: 모델정보, 엔진리스트 모델 수정

--- src/model/car/Info/ModelInfo.ts
+++ src/model/car/Info/ModelInfo.ts
@@ -1,16 +1,20 @@
 export interface ModelInfo {
   brandName: string;
+  detailModelPk: string;
   detailModelName: string;
   detailModelMainImage: string;
   detailModelNormalImages: string[];
   detailModelColorImages: string[];
   detailModelSpec: {
+    carClass: string;
+    fuelTypes: string[];
+    maxDiscountPercent: number;
     minCarPrice: number;
+    maxCarPrice: number;
     minEngineDisplacement: number;
     maxEngineDisplacement: number;
     minFuelEfficiency: number;
     maxFuelEfficiency: number;
-    fuelTypes: string[];
-    carClass: string;
+    priority: number;
   };
 }
